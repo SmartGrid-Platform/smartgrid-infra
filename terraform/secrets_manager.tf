fe4c410eb87d0bb9651c8bdf38be7829a3100984
Likhi161 <b.likhitha161@gmail.com>
2026-06-09 12:24:25 +0530

    feat: automate configuration population in Secrets Manager using HCL secret version

--- terraform/secrets_manager.tf
+++ terraform/secrets_manager.tf
@@ -7,3 +7,23 @@ resource "aws_secretsmanager_secret" "smartgrid_secret" {
   description             = "Database credentials and config variables for SmartGrid microservices"
   recovery_window_in_days = 0 # Force delete immediately on destroy
 }
+
+resource "aws_secretsmanager_secret_version" "smartgrid_secret_val" {
+  secret_id     = aws_secretsmanager_secret.smartgrid_secret.id
+  secret_string = jsonencode({
+    NODE_ENV       = "production"
+    DB_HOST        = aws_instance.database.private_ip
+    DB_PORT        = "3306"
+    DB_USER        = "smartgrid_user"
+    DB_PASSWORD    = "password"
+    DB_NAME        = "smartgrid"
+    JWT_SECRET     = "a2b53cdd87431e5630283c448f72ee7b2c91b5da8d1234c9fb66b3f7efc4901f"
+    SMTP_HOST      = "smtp.mailtrap.io"
+    SMTP_PORT      = "2525"
+    SMTP_USER      = ""
+    SMTP_PASS      = ""
+    SENDER_EMAIL   = "noreply@smartgrid.com"
+    S3_BUCKET_NAME = aws_s3_bucket.bills_bucket.id
+    AWS_REGION     = var.aws_region
+  })
+}
